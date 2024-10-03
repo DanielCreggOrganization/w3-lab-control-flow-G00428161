3a80dc1979a6f22dceb8d8fe9f6b1b780de2ec75
G00428161 <145337544+G00428161@users.noreply.github.com>
2024-10-03 15:24:17 +0000

    Lab 3

---
+++ src/ie/atu/intro/ControlFlowExample.java
@@ -0,0 +1,13 @@
+package ie.atu.intro;
+
+public class ControlFlowExample {
+    public static void main(String[] args) {
+        int number = 100;
+
+        if (number % 4 == 0) {
+            System.out.println("The number is a leap year.");
+        } else {
+            System.out.println("The number is not a leap year.");
+        }
+    }
+}
